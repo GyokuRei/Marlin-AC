02b629f00a28dd9cfc2d8115e1042253bb09e716
LVD-AC <lvd@sound-silence.com>
2017-09-27 21:43:34 +0200

    bug

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -5427,6 +5427,7 @@ void home_all_axes() { gcode_G28(true); }
                  _7p_no_center        = probe_points == 6 || probe_points == 9,
                  _7p_double_circle    = probe_points >= 5 && probe_points <= 7,
                  _7p_tripple_circle   = probe_points >= 8,
+                 _7p_intermediate     = probe_points >= 6,
                  _7p_half_blast       = probe_points == 8,
                  _7p_full_blast       = probe_points == 9,
                  _7p_zigzag_1_2       = probe_points == 5 || probe_points == 9,
@@ -5564,9 +5565,9 @@ void home_all_axes() { gcode_G28(true); }
               z_at_pt[axis] /= (2 * offset_circles + 1);
             }
           }
-          if (_7p_double_circle || _7p_tripple_circle) // add intermediates to 12p circle
+          if (_7p_intermediate) // add intermediates to 12p circle
             for (uint8_t axis = 1; axis < 25; axis += 2)
-             z_at_pt[axis] = (z_at_pt[axis] + (z_at_pt[axis + 1] + z_at_pt[(axis + 22) % 24 + 1]) / 2.0);
+             z_at_pt[axis] = (z_at_pt[axis] + (z_at_pt[axis + 1] + z_at_pt[(axis + 22) % 24 + 1]) / 2.0) / 2.0;
         }
         float S1 = z_at_pt[0],
               S2 = sq(z_at_pt[0]);
